a03237f02d91e6ac3b4d2323270ec368e8f92e94
Johnny Cai <jaihysc@gmail.com>
2019-07-17 08:41:43 -0400

    Client - Added schedule view UI functionality

--- TVClock Client/app/scheduleManager.js
+++ TVClock Client/app/scheduleManager.js
@@ -1,53 +1,34 @@
 "use strict";
 //Renderer
-var period = /** @class */ (function () {
-    function period(periodHour, periodIndex) {
-        this.periodType = "None"; //Default text on startup
-        this.periodHour = periodHour;
-        this.periodIndex = periodIndex;
+var scheduleItem = /** @class */ (function () {
+    function scheduleItem(periodHour, periodIndex) {
+        this.periodName = "None"; //Default text on startup
+        this.hour = periodHour;
+        this.index = periodIndex;
     }
-    return period;
+    return scheduleItem;
 }());
 //Generate time list
+//List of all 24 scheduleItems
+var scheduleItems = [];
 var timetable = $("#view-schedule-timetable");
 //AM
 for (var i = 1; i <= 12; ++i) {
-    timeTableAppend(new period(i + " AM", i - 1));
+    timeTableAppend(new scheduleItem(i + " AM", i - 1));
 }
 //PM
 for (var i = 1; i <= 12; ++i) {
-    timeTableAppend(new period(i + " PM", i - 1 + 12));
+    timeTableAppend(new scheduleItem(i + " PM", i - 1 + 12));
 }
 //Schedule clickable functionality
-var scheduleItems = $(".list-time-item");
 var selectedScheduleItemIndex = -1;
-var _loop_1 = function (i) {
-    scheduleItems[i].addEventListener("click", function () {
-        //Set clicked button as active
-        //The class list-group-item is added as active only shows up with list-group-item
-        //Removing the custom class list-group-item-darker
-        if (selectedScheduleItemIndex >= 0) {
-            scheduleItems[selectedScheduleItemIndex].classList.remove("active", "list-group-item");
-            scheduleItems[selectedScheduleItemIndex].classList.add("list-group-item-darker");
-        }
-        //If clicking the same item, unselect it
-        if (selectedScheduleItemIndex == i) {
-            selectedScheduleItemIndex = -1;
-        }
-        else {
-            selectedScheduleItemIndex = i;
-            scheduleItems[i].classList.add("active", "list-group-item");
-            scheduleItems[i].classList.remove("list-group-item-darker");
-        }
-    });
-};
-for (var i = 0; i < scheduleItems.length; ++i) {
-    _loop_1(i);
-}
-//Period clickable functionality
+//Whether or not the user has selected a scheduleItem
+var scheduleItemSelected = false;
 var periodItems = $(".list-period-item");
-var selectedPeriodItemIndex = -1;
-var _loop_2 = function (i) {
+var selectedPeriodItemIndex = -1; //-1 indicates nothing is selected
+var periodConfigurationMenu = $("#period-configuration-menu");
+periodConfigurationMenu.hide();
+var _loop_1 = function (i) {
     periodItems[i].addEventListener("click", function () {
         //Set clicked button as active
         //The class list-group-item is added as active only shows up with list-group-item
@@ -56,23 +37,103 @@ var _loop_2 = function (i) {
             periodItems[selectedPeriodItemIndex].classList.remove("active", "list-group-item");
             periodItems[selectedPeriodItemIndex].classList.add("list-group-item-darker");
         }
-        if (selectedPeriodItemIndex == i) {
+        //Allow unselecting of period items only when configurating periods
+        if (!scheduleItemSelected && selectedPeriodItemIndex == i) {
             selectedPeriodItemIndex = -1;
+            periodConfigurationMenu.hide();
         }
         else {
             selectedPeriodItemIndex = i;
             periodItems[i].classList.add("active", "list-group-item");
             periodItems[i].classList.remove("list-group-item-darker");
+            //Allow choosing period of a scheduleItem if a scheduleItem is selected
+            if (scheduleItemSelected) {
+                //Set the value of the scheduleItem to the period
+                scheduleItems[selectedScheduleItemIndex].periodName = periodItems[selectedPeriodItemIndex].innerHTML;
+                //Refresh for text changes to the schedule list to show up
+                timeTableRefresh();
+                periodConfigurationMenu.hide();
+            }
+            else { //Otherwise show the configuration menu for periods
+                periodConfigurationMenu.show();
+            }
         }
     });
 };
+//Period clickable functionality
 for (var i = 0; i < periodItems.length; ++i) {
-    _loop_2(i);
+    _loop_1(i);
 }
-function timeTableAppend(period) {
+timeTableRefresh();
+//
+function deselectAllPeriods() {
+    selectedPeriodItemIndex = -1;
+    for (var i = 0; i < periodItems.length; ++i) {
+        periodItems[i].classList.remove("active", "list-group-item");
+        periodItems[i].classList.add("list-group-item-darker");
+    }
+}
+function timeTableAppend(item) {
+    scheduleItems.push(item);
     timetable.append("<li class=\"list-group-item-darker list-group-flush list-time-item\">" +
         "<div class=\"row\"> <div class=\"col-2\"> " + //My apologies for this ugly html, but I assure you it works ;)
-        "<p class=\"\">" + period.periodHour + "</p> </div> " +
-        "<div class=\"col-10\"> <p class=\"\">" + period.periodType +
+        "<p class=\"\">" + item.hour + "</p> </div> " +
+        "<div class=\"col-10\"> <p class=\"\">" + item.periodName +
         "</p> </div> </div> </li>");
+}
+function timeTableRefresh() {
+    timetable.html(" "); //Clear old text
+    for (var i = 0; i < scheduleItems.length; ++i) {
+        timetable.append("<li class=\"list-group-item-darker list-group-flush list-time-item\">" +
+            "<div class=\"row\"> <div class=\"col-2\"> " + //My apologies for this ugly html, but I assure you it works ;)
+            "<p class=\"\">" + scheduleItems[i].hour + "</p> </div> " +
+            "<div class=\"col-10\"> <p class=\"\">" + scheduleItems[i].periodName +
+            "</p> </div> </div> </li>");
+    }
+    //Setup schedule item click handlers
+    var htmlScheduleItems = $(".list-time-item");
+    var _loop_2 = function (i) {
+        htmlScheduleItems[i].addEventListener("click", function () {
+            //Hide period config menu
+            periodConfigurationMenu.hide();
+            //Set clicked button as active
+            //The class list-group-item is added as active only shows up with list-group-item
+            //Removing the custom class list-group-item-darker
+            if (selectedScheduleItemIndex >= 0) {
+                htmlScheduleItems[selectedScheduleItemIndex].classList.remove("active", "list-group-item");
+                htmlScheduleItems[selectedScheduleItemIndex].classList.add("list-group-item-darker");
+            }
+            //If clicking the same item, unselect it
+            if (selectedScheduleItemIndex == i) {
+                selectedScheduleItemIndex = -1;
+                scheduleItemSelected = false;
+                deselectAllPeriods();
+            }
+            else {
+                selectedScheduleItemIndex = i;
+                htmlScheduleItems[i].classList.add("active", "list-group-item");
+                htmlScheduleItems[i].classList.remove("list-group-item-darker");
+                //Select scheduleItem item associated with the schedule
+                scheduleItemSelected = true;
+                //Select the period item matching the schedule item
+                for (var j = 0; j < periodItems.length; ++j) {
+                    //Find the desired period based on provided periodName
+                    if (periodItems[j].innerHTML == scheduleItems[i].periodName) {
+                        //If it is found, select it if it is not already selected
+                        if (j == selectedPeriodItemIndex)
+                            return;
+                        periodItems[j].click();
+                    }
+                }
+            }
+        });
+    };
+    for (var i = 0; i < htmlScheduleItems.length; ++i) {
+        _loop_2(i);
+    }
+    //Select the previously selected task
+    if (selectedScheduleItemIndex >= 0) {
+        htmlScheduleItems[selectedScheduleItemIndex].classList.add("active", "list-group-item");
+        htmlScheduleItems[selectedScheduleItemIndex].classList.remove("list-group-item-darker");
+    }
 }
--- TVClock Client/typescript/scheduleManager.ts
+++ TVClock Client/typescript/scheduleManager.ts
@@ -1,56 +1,42 @@
 //Renderer
 
+class scheduleItem {
+    periodName: string = "None"; //Default text on startup
 
-class period {
-    periodType: string = "None"; //Default text on startup
-
-    periodHour: string; // E.G 1 AM
-    periodIndex: number; //index, 0 based for internal ue
+    hour: string; // E.G 1 AM
+    index: number; //index, 0 based for internal ue
 
     constructor(periodHour: string, periodIndex: number) {
-        this.periodHour = periodHour;
-        this.periodIndex = periodIndex;
+        this.hour = periodHour;
+        this.index = periodIndex;
     }
 }
 
 //Generate time list
+//List of all 24 scheduleItems
+let scheduleItems: scheduleItem[] = [];
 let timetable = $("#view-schedule-timetable");
 //AM
 for (let i = 1; i <= 12; ++i) {
-    timeTableAppend(new period(i + " AM", i - 1));
+    timeTableAppend(new scheduleItem(i + " AM", i - 1));
 }
 //PM
 for (let i = 1; i <= 12; ++i) {
-    timeTableAppend(new period(i + " PM", i - 1 + 12));
+    timeTableAppend(new scheduleItem(i + " PM", i - 1 + 12));
 }
 
 //Schedule clickable functionality
-let scheduleItems = $(".list-time-item");
 let selectedScheduleItemIndex = -1;
-for (let i = 0; i < scheduleItems.length; ++i) {
-    scheduleItems[i].addEventListener("click", () => {
-        //Set clicked button as active
-        //The class list-group-item is added as active only shows up with list-group-item
-        //Removing the custom class list-group-item-darker
-        if (selectedScheduleItemIndex >= 0) {
-            scheduleItems[selectedScheduleItemIndex].classList.remove("active", "list-group-item");
-            scheduleItems[selectedScheduleItemIndex].classList.add("list-group-item-darker");
-        }
 
-        //If clicking the same item, unselect it
-        if (selectedScheduleItemIndex == i) {
-            selectedScheduleItemIndex = -1;
-        } else {
-            selectedScheduleItemIndex = i;
-            scheduleItems[i].classList.add("active", "list-group-item");
-            scheduleItems[i].classList.remove("list-group-item-darker");
-        }
-    })
-}
+//Whether or not the user has selected a scheduleItem
+let scheduleItemSelected = false;
 
-//Period clickable functionality
 let periodItems = $(".list-period-item");
-let selectedPeriodItemIndex = -1;
+let selectedPeriodItemIndex = -1; //-1 indicates nothing is selected
+
+let periodConfigurationMenu = $("#period-configuration-menu");
+periodConfigurationMenu.hide();
+//Period clickable functionality
 for (let i = 0; i < periodItems.length; ++i) {
     periodItems[i].addEventListener("click", () => {
         //Set clicked button as active
@@ -61,20 +47,110 @@ for (let i = 0; i < periodItems.length; ++i) {
             periodItems[selectedPeriodItemIndex].classList.add("list-group-item-darker");
         }
 
-        if (selectedPeriodItemIndex == i) {
+        //Allow unselecting of period items only when configurating periods
+        if (!scheduleItemSelected && selectedPeriodItemIndex == i) {
             selectedPeriodItemIndex = -1;
+            periodConfigurationMenu.hide();
         } else {
             selectedPeriodItemIndex = i;
             periodItems[i].classList.add("active", "list-group-item");
             periodItems[i].classList.remove("list-group-item-darker");
+
+            //Allow choosing period of a scheduleItem if a scheduleItem is selected
+            if (scheduleItemSelected) {
+                //Set the value of the scheduleItem to the period
+                scheduleItems[selectedScheduleItemIndex].periodName = periodItems[selectedPeriodItemIndex].innerHTML;
+
+                //Refresh for text changes to the schedule list to show up
+                timeTableRefresh();
+
+                periodConfigurationMenu.hide();
+            } else { //Otherwise show the configuration menu for periods
+                periodConfigurationMenu.show();
+            }
         }
     })
 }
 
-function timeTableAppend(period: period) {
+timeTableRefresh();
+
+//
+
+function deselectAllPeriods() {
+    selectedPeriodItemIndex = -1;
+    for (let i = 0; i < periodItems.length; ++i) {
+        periodItems[i].classList.remove("active", "list-group-item");
+        periodItems[i].classList.add("list-group-item-darker");
+    }
+}
+
+function timeTableAppend(item: scheduleItem) {
+    scheduleItems.push(item);
     timetable.append("<li class=\"list-group-item-darker list-group-flush list-time-item\">" +
         "<div class=\"row\"> <div class=\"col-2\"> " + //My apologies for this ugly html, but I assure you it works ;)
-        "<p class=\"\">" + period.periodHour + "</p> </div> " +
-        "<div class=\"col-10\"> <p class=\"\">" + period.periodType +
+        "<p class=\"\">" + item.hour + "</p> </div> " +
+        "<div class=\"col-10\"> <p class=\"\">" + item.periodName +
         "</p> </div> </div> </li>")
+}
+
+function timeTableRefresh() {
+    timetable.html(" "); //Clear old text
+    for (let i = 0; i < scheduleItems.length; ++i) {
+        timetable.append("<li class=\"list-group-item-darker list-group-flush list-time-item\">" +
+            "<div class=\"row\"> <div class=\"col-2\"> " + //My apologies for this ugly html, but I assure you it works ;)
+            "<p class=\"\">" + scheduleItems[i].hour + "</p> </div> " +
+            "<div class=\"col-10\"> <p class=\"\">" + scheduleItems[i].periodName +
+            "</p> </div> </div> </li>")
+    }
+
+    //Setup schedule item click handlers
+    let htmlScheduleItems = $(".list-time-item");
+    for (let i = 0; i < htmlScheduleItems.length; ++i) {
+        htmlScheduleItems[i].addEventListener("click", () => {
+            //Hide period config menu
+            periodConfigurationMenu.hide();
+
+            //Set clicked button as active
+            //The class list-group-item is added as active only shows up with list-group-item
+            //Removing the custom class list-group-item-darker
+            if (selectedScheduleItemIndex >= 0) {
+                htmlScheduleItems[selectedScheduleItemIndex].classList.remove("active", "list-group-item");
+                htmlScheduleItems[selectedScheduleItemIndex].classList.add("list-group-item-darker");
+            }
+
+            //If clicking the same item, unselect it
+            if (selectedScheduleItemIndex == i) {
+                selectedScheduleItemIndex = -1;
+
+                scheduleItemSelected = false;
+
+                deselectAllPeriods();
+            } else {
+                selectedScheduleItemIndex = i;
+                htmlScheduleItems[i].classList.add("active", "list-group-item");
+                htmlScheduleItems[i].classList.remove("list-group-item-darker");
+
+                //Select scheduleItem item associated with the schedule
+                scheduleItemSelected = true;
+
+                //Select the period item matching the schedule item
+                for (let j = 0; j < periodItems.length; ++j) {
+                    //Find the desired period based on provided periodName
+                    if (periodItems[j].innerHTML == scheduleItems[i].periodName) {
+                        //If it is found, select it if it is not already selected
+                        if (j == selectedPeriodItemIndex)
+                            return;
+
+                        periodItems[j].click();
+                    }
+                }
+            }
+        })
+    }
+
+    //Select the previously selected task
+    if (selectedScheduleItemIndex >= 0) {
+        htmlScheduleItems[selectedScheduleItemIndex].classList.add("active", "list-group-item");
+        htmlScheduleItems[selectedScheduleItemIndex].classList.remove("list-group-item-darker");
+    }
 }
