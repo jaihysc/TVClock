8a00fe8d819f013e9a73025b0069a82e7d179e83
Johnny Cai <jaihysc@gmail.com>
2019-07-24 07:17:39 -0400

    Removed compiled js files from source control

--- TVClock Client/app/renderer.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-require("./statusBarManager.js");
-require("./viewManager.js");
--- TVClock Client/typescript/scripts.ts
+++ TVClock Client/typescript/scripts.ts
@@ -1,3 +1,5 @@
 //Loads external javascript libraries
+// @ts-ignore
 window.$ = window.jQuery = require('jquery');
+// @ts-ignore
 window.Bootstrap = require('bootstrap');
